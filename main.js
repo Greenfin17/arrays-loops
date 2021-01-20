a46b61e6737950c66dbc1543bb0dc39aa44abde3
Greenfin17 <jmaple@yahoo.com>
2021-01-19 20:38:23 -0600

    did some array methods

--- main.js
+++ main.js
@@ -5,47 +5,22 @@ function showDate() {
   console.log("Today's date is " + today);
 }
 
-//console.log("Connected");
-// for(let i = 0; i <=15; i++) {
-//   console.log(i);
-// }
-
-// const functionforTad = () => {
-//  for(let i = 0; i <=15; i++) {
-//    console.log(i);
-//  }
-// }
-//  functionforTad();
-
-//  // ************* ARRAYS WITH LOOPS *************
-//  const ec14Instructors = ['Aja','Trinity', 'Dr.T', 'Discussion Tickets'];
-//  for(let i = 0 ; i < ec14Instructors.length; i++) {
-//    console.log(ec14Instructors[i]);
-//  }
+const array1 = ['cow', 'dog', 'cat'];
+const string1 = array1.join(', ');
+console.log(string1);
+const strint1ToArray  = string1.split(', ');
+console.log(strint1ToArray);
 
 
- // *************** ARRAYS, LOOPS, FUNCTIONS ***************
+// .push() &  .pop()
+array1.pop();
+console.log(array1);
+array1.push('cat');
+console.log(array1);
 
- const colors = ['red','orange','yellow','green','blue','purple'];
- const colorLoop = () => {
-   let domString ='';
-   for (let i =0 ; i < colors.length; i++) {
-     domString += `<h1>${colors[i]}</h1>`;
-   }
-   console.log(domString);
- }
-colorLoop();
- const instructors = [
-   { first: 'Trinity', last: 'Christiana' },
-   { first: 'Aja', last: 'Washington' },
-   { first: 'Teresa', last: 'Vasquez' },
- ];
+// .unshift() & .shift()
 
-const nameLoop = () => {
-  let domString = '';
-  for(let i = 0; i < instructors.length; i++) {
-    domString += `<h1>${instructors[i].first} ${instructors[i].last}</h1>`;
-  }
-  console.log(domString);
-}
-nameLoop();
+array1.unshift('cat');
+console.log(array1);
+array1.shift();
+console.log(array1);
